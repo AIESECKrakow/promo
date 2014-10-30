2df767c17c8551f7aa3efa52c7655a0271ab7429
ZyzioZiom <patryk.kalinowski@gmail.com>
2014-10-30 23:35:42 +0100

    Intro na logowanie

--- landing.php
+++ landing.php
@@ -1,18 +1,3 @@
-﻿<html>
-<head><title>Promo Platform</title>
-<meta charset="UTF-8">
-<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
-<link href="theme/smurfs/style.css" type="text/css" rel="stylesheet" />
-</head>
-<body style="margin: 0px; padding: 0px; background-color: #1e4394;">
 
-<div class="landing"> </div>
- <div class="sign">            <div class="login_sign">
-<?
+<?php include 'theme/smurfs/index.php';?>
 
-echo "<a class='login' href='$authUrl'><img src=\"google-login-button-asif18.png\" alt=\"Google login using php api for your website\" title=\"login with google\" /></a>";
-
-?> </div>   </div>
-
- </body>
-</html>
---
+++ theme/smurfs/index.php
@@ -0,0 +1,76 @@
+<?php
+
+echo "<!DOCTYPE html>
+<html>
+	<head>
+	<title>Promo Platform</title>
+<meta charset=\"UTF-8\">
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
+<link href=\"theme/smurfs/style.css\" type=\"text/css\" rel=\"stylesheet\" />
+	<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap.min.css\">	
+	</head>
+	<body style=\"margin: 0px; padding: 0px; \">
+	
+	<body style=\"margin: 0px; padding: 0px; width:100% background-color: #1e4394;\">
+
+<div class=\"landing\"> </div>
+
+<div class=\"smerfowanie\">
+<img src=\"theme/smurfs/img/papasmerf.png\" class='image'/>
+<h2 style='color:white; top:50%;'>Smerfowanie promoplatformy...</h2>
+</div>
+
+
+ <div class=\"sign\">            <div class=\"login_sign\">
+
+
+<a class='login' href='$authUrl'><img src=\"google-login-button-asif18.png\" alt=\"Google login using php api for your website\" title=\"login with google\" /></a>
+<img id=\"speaker\" src=\"theme/smurfs/img/speaker.png\" style=\"height:30px\" />
+
+ </div>   </div>
+
+	<div class=\"row col-md-12\" style=\"text-align:center;\" style=\"display: table;\">
+	<div style=\"display: table-cell; vertical-align: middle;\">
+	<video id=\"video\" autoplay oncanplaythrough=\"canPlayThrough()\">
+		  <source src=\"theme/smurfs/img/intro.mp4\" type=\"video/mp4\" >
+		  
+
+		
+		</video>
+		</div>
+		</div>
+		<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src=\"https://cdn.jsdelivr.net/bootstrap/3.3.0/js/bootstrap.min.js\"></script>
+
+<script>
+// wyciszanie filmiku
+$('#speaker').click(function() {
+
+if ($('#video').prop('muted')) 
+{
+$('#video').prop('muted', false); //unmute
+console.log('unmute');
+}
+else {
+$('#video').prop('muted', true); //mute
+console.log('mute');
+}
+});
+</script>
+
+<script>
+// uruchomienie filmu, div ładowania znika po sekundzie
+function canPlayThrough() {
+console.log('canplaythrough');
+setTimeout(function() {
+$('.smerfowanie').fadeOut('slow');
+}, 1000);
+}
+</script>
+		</body>
+		</head>
+		</html>
+		"
+		
+		?>
